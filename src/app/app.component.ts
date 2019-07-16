bb23b6aa134c0a1fd6a032ca5982571503a49e23
Sayed Wakil <syedawakil@gmail.com>
2019-07-17 03:00:26 +1000

    added normalisation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,19 +8,131 @@ import * as _ from 'lodash';
 })
 
 export class AppComponent {
+
+  jsonobj: any = [{ "bands": [{ "name": "Propeller", "recordLabel": "Pacific Records" }, { "name": "Critter Girls", "recordLabel": "ACR" }] }, { "name": "Twisted Tour", "bands": [{ "name": "Squint-281" }, { "name": "Summon", "recordLabel": "Outerscope" }, { "name": "Auditones", "recordLabel": "Marner Sis. Recording" }] }, { "name": "Trainerella", "bands": [{ "name": "Manish Ditch", "recordLabel": "ACR" }, { "name": "Adrian Venti", "recordLabel": "Monocracy Records" }, { "name": "Wild Antelope", "recordLabel": "Still Bottom Records" }, { "name": "YOUKRANE", "recordLabel": "Anti Records" }] }, { "name": "LOL-palooza", "bands": [{ "name": "Werewolf Weekday", "recordLabel": "XS Recordings" }, { "name": "Winter Primates", "recordLabel": "" }, { "name": "Jill Black", "recordLabel": "Fourth Woman Records" }, { "name": "Frank Jupiter", "recordLabel": "Pacific Records" }] }, { "name": "Small Night In", "bands": [{ "name": "Green Mild Cold Capsicum", "recordLabel": "Marner Sis. Recording" }, { "name": "The Black Dashes", "recordLabel": "Fourth Woman Records" }, { "name": "Squint-281", "recordLabel": "Outerscope" }, { "name": "Wild Antelope", "recordLabel": "Marner Sis. Recording" }, { "name": "Yanke East", "recordLabel": "MEDIOCRE Music" }] }];
+  //jsonobj: any = [{ "bands": [{ "name": "Propeller", "recordLabel": "Pacific Records" }, { "name": "Critter Girls", "recordLabel": "ACR" }] }, { "name": "Twisted Tour", "bands": [{ "name": "Squint-281" }, { "name": "Summon", "recordLabel": "Outerscope" }, { "name": "Auditones", "recordLabel": "Marner Sis. Recording" }] }, { "name": "Trainerella", "bands": [{ "name": "Summon", "recordLabel": "Outerscope" },{ "name": "Manish Ditch", "recordLabel": "ACR" }, { "name": "Adrian Venti", "recordLabel": "Monocracy Records" }, { "name": "Wild Antelope", "recordLabel": "Still Bottom Records" }, { "name": "YOUKRANE", "recordLabel": "Anti Records" }] }, { "name": "LOL-palooza", "bands": [{ "name": "Werewolf Weekday", "recordLabel": "XS Recordings" }, { "name": "Winter Primates", "recordLabel": "" }, { "name": "Jill Black", "recordLabel": "Fourth Woman Records" }, { "name": "Frank Jupiter", "recordLabel": "Pacific Records" }] }, { "name": "Small Night In", "bands": [{ "name": "Green Mild Cold Capsicum", "recordLabel": "Marner Sis. Recording" }, { "name": "The Black Dashes", "recordLabel": "Fourth Woman Records" }, { "name": "Squint-281", "recordLabel": "Outerscope" }, { "name": "Wild Antelope", "recordLabel": "Marner Sis. Recording" }, { "name": "Yanke East", "recordLabel": "MEDIOCRE Music" }] }];
  
+  recordLabelAry = [];
+  masterLabelAry = [];
+
+ 
+
+  bandObj = {
+    bandname: "",
+    festivals: []
+  }
+
 
   constructor() { }
 
   ngOnInit() {
-    this.lodashTest()
+    this.getDistinctRecordLabels();
+    this.normalizeData();
+    //this.lodashTest()
   }
 
-  lodashTest(){
+  lodashTest() {
     let str: string = "testing 34 dollars";
     let aryStr = _.words(str);
-    console.log("arystr",aryStr);
+    console.log("arystr", aryStr);
+    console.log("Json obj", this.jsonobj);
+    this.getDistinctRecordLabels();
+    this.normalizeData();
+
+  }
+
+  isInArray(array, name) {
+
+    return array.indexOf(name.toLowerCase()) > -1;
+  }
+
+  getDistinctRecordLabels() {
+
+    _.forEach(this.jsonobj, festival => {
+     
+      _.forEach(festival.bands, band => {
+
+        let banddObj = {
+          band: "",
+          festival: ""
+        };
+
+        if (band.recordLabel) {
+          //if (!this.isInArray(this.recordLabelAry, band.recordLabel)) {
+            this.recordLabelAry.push(band.recordLabel);
+          //}
+
+     
+        }
+
+
+
+      });
+
+
+    });
+
+    //removing duplicates
+    this.recordLabelAry = this.recordLabelAry.filter((el, i, a) => i === a.indexOf(el))
     
+
+  }
+
+
+
+  normalizeData() {
+    for(let i=0; i<this.recordLabelAry.length; i++) {
+
+      let recordLabeltemp = this.recordLabelAry[i];
+      let  recordLabelObj = {
+        recordLabelname: recordLabeltemp,
+        bands: []
+      }
+
+      _.forEach(this.jsonobj, festival => {
+        
+        _.forEach(festival.bands, band => {
+  
+          let banddObj = {
+            band: band.name,
+            festival: ""
+          };
+  
+          if (band.recordLabel && band.recordLabel ===recordLabeltemp)  {
+          
+            //banddObj.band = band.name;
+            if(festival.name) {
+              banddObj.festival = festival.name;
+            }
+              else {
+                banddObj.festival = 'n/a'
+              }
+            
+              recordLabelObj.bands.push(banddObj);
+          }
+
+          
+
+
+  
+  
+  
+        });
+  
+  
+      });
+      this.masterLabelAry.push(recordLabelObj);
+
+
+
+    }
+
+    
+
+    //sorting next -->
+
+
+
   }
 
 
